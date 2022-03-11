7cab32b2a26db250fc66aee5f539a0cc3f04d09f
birisdiana8 <91530536+birisdiana8@users.noreply.github.com>
2022-03-11 14:19:19 +0200

    Add files via upload

---
+++ Seminar3/src/solid/i/resolved/HPBasicModel.java
@@ -0,0 +1,17 @@
+package solid.i.resolved;
+
+public class HPBasicModel implements iPrintTask, iScanTask{
+
+	@Override
+	public boolean printContent(String content) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scanContent(String content) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Seminar3/src/solid/i/resolved/iPrintTask.java
@@ -0,0 +1,11 @@
+package solid.i.resolved;
+
+public interface iPrintTask {
+	
+	boolean printContent(String content);
+//	boolean scanContent(String content);
+//	boolean faxContent(String content);
+//	boolean printContentExternalDevice(String content);
+//	boolean authBadge(String content);
+
+}
