e3e2b71234fc0f37cec5c86b716fa63d5e69b34f
birisdiana8 <91530536+birisdiana8@users.noreply.github.com>
2022-03-27 18:16:57 +0300

    Add files via upload

---
+++ Seminar4/src/solid/Calculator.java
@@ -0,0 +1,23 @@
+package solid;
+
+public class Calculator {
+	
+	private enum Operations{ ADD, ABSTRACT, MULTIPLY, DIVIDE};
+	
+	Calculator(){
+	
+	}
+	
+	public double calculate(double a, double b, int i) {
+		switch(i) {
+		case 1:
+			return a+b;
+			//break;
+		case 2:
+			return a-b;
+		default: 
+			return 0;
+		}
+	}
+
+}
