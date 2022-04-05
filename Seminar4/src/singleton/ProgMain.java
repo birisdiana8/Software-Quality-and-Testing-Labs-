4a21f84e9a99428fde412a726f2d03ff3376e6bb
birisdiana8 <91530536+birisdiana8@users.noreply.github.com>
2022-04-05 21:07:56 +0300

    Update ProgMain.java

--- Seminar4/src/singleton/ProgMain.java
+++ Seminar4/src/singleton/ProgMain.java
@@ -4,10 +4,13 @@ public class ProgMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//lazy initialization
+				DatabaseLazyInitialization db1=DatabaseLazyInitialization.getInstance();	//creates the instance
+				DatabaseLazyInitialization db2=DatabaseLazyInitialization.getInstance(); //checks if instance is null (no)-> just returns the instance
 
-		
-		Database db1 = Database.getInstance();
-		Database db2 = Database.getInstance();
+				//early initialization
+				DatabaseEarlyInitialization d3;
+				DatabaseEarlyInitialization.getInstance();
 	}
 
 }
